1d032ea1eac66522cea2c3a0e907e21adabdb68b
leo <leo@leo-laptop>
2009-08-31 21:14:59 +0300

    Initial

---
+++ apps/frontend/modules/index/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * index actions.
+ *
+ * @package    symfony_films
+ * @subpackage index
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 12479 2008-10-31 10:54:40Z fabien $
+ */
+class indexActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+}
