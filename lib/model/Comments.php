b1307332379fe49705fdf7054a28be190cc9ad0d
le0pard <leopard.not.a@gmail.com>
2009-11-25 10:31:10 +0200

    Fix comments

--- lib/model/Comments.php
+++ lib/model/Comments.php
@@ -2,14 +2,14 @@
 
 class Comments extends BaseComments
 {	
-	static public function getCacheArray(){
-		return array(
-			'comment/last_comments',
-			'@sf_cache_partial?module=comment&action=_last_comments&sf_cache_key=last_comments'
-		);
-	}
+
 }
 
-sfPropelBehavior::add('FilmTypes', array(
-	'viewCacheObserver'
+sfPropelBehavior::add('Comments', array(
+	'viewCacheObserver' => array(
+		'cache' => array(
+			'comment/last_comments',
+			'@sf_cache_partial?module=comment&action=_last_comments&sf_cache_key=last_comments'
+		),
+	)
 ));
