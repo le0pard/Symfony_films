bd7a49a62bf7d5434ba1fca5c656fec5a31c33b4
le0pard <leopard.not.a@gmail.com>
2009-11-28 00:26:09 +0200

    Fix comment form

--- lib/form/CommentsForm.class.php
+++ lib/form/CommentsForm.class.php
@@ -23,9 +23,10 @@ class CommentsForm extends BaseCommentsForm
 	  				array('theme' => 'advanced','config' => '
 							skin : "o2k7", 
 							language : "ru",
-							plugins : "safari,spellchecker,table,advimage,advlink,searchreplace,contextmenu,paste,directionality,fullscreen",
-							theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,formatselect,fontselect,fontsizeselect",
-							theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code,|,forecolor,backcolor",
+							plugins : "safari",
+							theme_advanced_buttons1 : "bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,code",
+							theme_advanced_buttons2 : "",
+							theme_advanced_buttons3 : "",
 							theme_advanced_toolbar_location : "top",
 							theme_advanced_toolbar_align : "left",
 							theme_advanced_statusbar_location : "bottom",
