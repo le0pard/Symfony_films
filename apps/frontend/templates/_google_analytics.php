6a316e3a7a18593b358501f7c177e38cbdb89ac5
le0pard <leopard.not.a@gmail.com>
2009-12-03 16:30:03 +0200

    New asynchronys GA

--- apps/frontend/templates/_google_analytics.php
+++ apps/frontend/templates/_google_analytics.php
@@ -1,10 +1,13 @@
 <script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("<?php echo sfConfig::get('app_google_analytics_code', "UA-10000-01");?>");
-pageTracker._trackPageview();
-} catch(err) {}
-</script>  
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', '<?php echo sfConfig::get('app_google_analytics_code', "UA-10000-01");?>']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script');
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  ga.setAttribute('async', 'true');
+  document.documentElement.firstChild.appendChild(ga);
+})();
+
+</script> 
