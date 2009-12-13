81a88a94814cca1807ea2fc921d617f3600b875b
le0pard <leopard.not.a@gmail.com>
2009-12-13 18:18:06 +0200

    Icons for jabber status

--- apps/frontend/modules/user/templates/showSuccess.php
+++ apps/frontend/modules/user/templates/showSuccess.php
@@ -4,7 +4,7 @@
 </div>
 <?php if (sfConfig::get('app_integration_is_jabber')): ?>
 <div>
-<img src="http://<?php echo sfConfig::get('app_integration_jabber_server'); ?>/plugins/presence/status?jid=<?php echo $user_data->getLogin() ?>@<?php echo sfConfig::get('app_integration_jabber_domain'); ?>" title="jabber status" />
+<img src="http://<?php echo sfConfig::get('app_integration_jabber_server'); ?>/plugins/presence/status?jid=<?php echo $user_data->getLogin() ?>@<?php echo sfConfig::get('app_integration_jabber_domain'); ?>&images=http://<?php echo sfConfig::get('app_domain'); ?>/images/jabber/--IMAGE--.gif" title="jabber status" />
 <?php echo $user_data->getLogin() ?>@<?php echo sfConfig::get('app_integration_jabber_domain'); ?>
 </div>
 <?php endif ?>
