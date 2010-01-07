1d1047edfea108be4ae4c597201f3b83c5a6c787
le0pard <leopard.not.a@gmail.com>
2010-01-07 16:22:16 +0200

    Fix google analytics

--- apps/frontend/templates/_google_analytics.php
+++ apps/frontend/templates/_google_analytics.php
@@ -1,13 +1,13 @@
 <script type="text/javascript">
+
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', '<?php echo sfConfig::get('app_google_analytics_code', "UA-10000-01");?>']);
+_gaq.push(['_setAccount', '<?php echo sfConfig::get('app_google_analytics_code', "UA-XXXXX-XX");?>']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-  var ga = document.createElement('script');
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  ga.setAttribute('async', 'true');
-  document.documentElement.firstChild.appendChild(ga);
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
 })();
 
 </script> 
