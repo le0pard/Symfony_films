7b2ed2e0db758e798a0da8c1468bbac81a647bd4
le0pard <leopard.not.a@gmail.com>
2010-01-07 16:25:20 +0200

    ^_^

--- lib/model/News.php
+++ lib/model/News.php
@@ -2,4 +2,12 @@
 
 class News extends BaseNews
 {
+	public function __toString(){
+    	return $this->getTitle();
+    }
+	
+	public function setTitle($title){
+	  parent::setTitle($title);
+	  $this->setUrl(System::slugify($title));
+	}
 }
