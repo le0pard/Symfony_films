66512c89f695bf229593f7bd8c789773b5fee22d
le0pard <leopard.not.a@gmail.com>
2010-01-23 23:21:07 +0200

    Form textarea

--- apps/frontend/modules/user/templates/profileSuccess.php
+++ apps/frontend/modules/user/templates/profileSuccess.php
@@ -1,7 +1,7 @@
 <h1>Редактирование профиля</h1>
 <form id="profile_form" action="<?php echo url_for('@user_profile') ?>" method="POST" <?php $form->isMultipart() and print 'enctype="multipart/form-data"' ?>>
   <table class="table_form">
-    <?php echo $form ?>
+  	<?php echo $form; ?>
     <tr>
       <td colspan="2">
         <input type="submit" value="Обновить" />
