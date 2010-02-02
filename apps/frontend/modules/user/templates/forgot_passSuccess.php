f322610cd5f3ea74f705be9575ca397e77a98c8d
le0pard <leopard.not.a@gmail.com>
2010-02-02 22:57:34 +0200

    Add captcha

--- apps/frontend/modules/user/templates/forgot_passSuccess.php
+++ apps/frontend/modules/user/templates/forgot_passSuccess.php
@@ -1,7 +1,27 @@
+<?php use_helper('sfCryptoCaptcha') ?>
 <h1>Напомнить пароль</h1>
 <form action="<?php echo url_for('@user_forgot_pass') ?>" method="POST" <?php $form->isMultipart() and print 'enctype="multipart/form-data"' ?>>
+  <?php if ($form->isCSRFProtected()) : ?> 
+  	<?php echo $form[$form->getCSRFFieldName()]->render(); ?>
+  <?php endif ?>
   <table class="table_form">
-    <?php echo $form ?>
+    <tr>
+      <th><?php echo $form['email']->renderLabel(); ?></th>
+      <td>
+        <?php echo $form['email']->renderError(); ?>
+        <?php echo $form['email']->render(); ?>
+		<?php echo $form['email']->renderHelp(); ?>
+      </td>
+    </tr>
+    <tr>
+      <th><?php echo $form['captcha']->renderLabel(); ?></th>
+      <td>
+        <?php echo $form['captcha']->renderError(); ?>
+		<?php echo captcha_image(); echo captcha_reload_button(); ?><br />
+        <?php echo $form['captcha']->render(); ?>
+		<?php echo $form['captcha']->renderHelp(); ?>
+      </td>
+    </tr>
     <tr>
       <td colspan="2">
         <input type="submit" value="Напомнить пароль" />
--- lib/form/ForgotPassForm.class.php
+++ lib/form/ForgotPassForm.class.php
@@ -4,13 +4,16 @@ class ForgotPassForm extends BaseForm{
   {
   	
     $this->setWidgets(array(
-      'email'    => new sfWidgetFormInputText()
+      'email'    => new sfWidgetFormInputText(),
+      'captcha'  => new sfWidgetFormInputText()
     ));
 	$this->widgetSchema->setLabels(array(
-	  'email'   => 'Email'
+	  'email'   => 'Email',
+	  'captcha' => 'Картинка'
 	));
 	$this->widgetSchema->setHelps(array(
-	  'email' => 'Введите email, кот орый вы указывали при регистрации.'
+	  'email' => 'Введите email, кот орый вы указывали при регистрации.',
+	  'captcha' => 'Повторите циферки на картинке.'
 	));
 	
 	$this->widgetSchema->setNameFormat('forgot_pass[%s]');
@@ -19,7 +22,10 @@ class ForgotPassForm extends BaseForm{
       'email'    => new sfValidatorAnd(array(
 				  		new sfValidatorString(array('required' => true, 'trim' => true), array('required' => 'Email должен быть указан.')),
 						new sfValidatorEmail(array(), array('invalid' => 'Email неверного формата.'))
-					), array('required' => true, 'trim' => true), array('required' => 'Email должен быть указан.'))
+					), array('required' => true, 'trim' => true), array('required' => 'Email должен быть указан.')),
+	  'captcha'  => new sfValidatorSfCryptoCaptcha(array('required' => true, 'trim' => true),
+                                                   array('wrong_captcha' => 'Неверные циферки.',
+                                                         'required' => 'Нам нужны эти циферки.'))
     ));
 	
 	$this->validatorSchema->setOption('allow_extra_fields', true);
