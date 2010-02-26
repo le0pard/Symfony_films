6c1a7b5e8d06cbee032c180f228c74703f3183db
leo <leo@leo-asus.local>
2010-02-27 01:18:41 +0200

    Fixtures

--- apps/frontend/modules/film_types/templates/_cathegory.php
+++ apps/frontend/modules/film_types/templates/_cathegory.php
@@ -1,6 +1,10 @@
 <div id="about_cat">
   <div id="hero">
-	<img src="/uploads/cathegory/<?php echo $cathegory->getLogo() ?>" alt="<?php echo $cathegory->getTitle() ?>" title="<?php echo $cathegory->getTitle() ?>" />
+  	<?php if ($cathegory->getLogo()):?>
+		<img src="/uploads/cathegory/<?php echo $cathegory->getLogo() ?>" alt="<?php echo $cathegory->getTitle() ?>" title="<?php echo $cathegory->getTitle() ?>" />
+	<?php else:?>
+		<img src="/uploads/cathegory/default/default.jpg" alt="<?php echo $cathegory->getTitle() ?>" title="<?php echo $cathegory->getTitle() ?>" />
+	<?php endif ?>
   </div>
   <div class="round" id="about_cat_text"><div class="r_container">
 	  <?php echo $cathegory->getDescription(ESC_RAW) ?><br />
