007ea346eaa127f36732d7ff9eeea1b1f81060b0
leo <leo@leo-asus.local>
2010-03-06 22:49:08 +0200

    Under work site

--- config/unavailable.php
+++ config/unavailable.php
@@ -1,44 +1,24 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-
-<?php $path = preg_replace('#/[^/]+\.php5?$#', '', isset($_SERVER['SCRIPT_NAME']) ? $_SERVER['SCRIPT_NAME'] : (isset($_SERVER['ORIG_SCRIPT_NAME']) ? $_SERVER['ORIG_SCRIPT_NAME'] : '')) ?>
-
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 
 <meta http-equiv="Content-Type" content="text/html; charset=<?php echo sfConfig::get('sf_charset', 'utf-8') ?>" />
-<meta name="title" content="symfony project" />
+<meta name="title" content="Coocoorooza" />
 <meta name="robots" content="index, follow" />
-<meta name="description" content="symfony project" />
-<meta name="keywords" content="symfony, project" />
+<meta name="description" content="Coocoorooza" />
+<meta name="keywords" content="Coocoorooza" />
 <meta name="language" content="en" />
-<title>symfony project</title>
+<title>Coocoorooza - Сайт на обслуживании</title>
 
 <link rel="shortcut icon" href="/favicon.ico" />
-<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $path ?>/sf/sf_default/css/screen.css" />
-<!--[if lt IE 7.]>
-<link rel="stylesheet" type="text/css" media="screen" href="<?php echo $path ?>/sf/sf_default/css/ie.css" />
-<![endif]-->
-
 </head>
 <body>
-<div class="sfTContainer">
-  <a title="symfony website" href="http://www.symfony-project.org/"><img alt="symfony PHP Framework" class="sfTLogo" src="<?php echo $path ?>/sf/sf_default/images/sfTLogo.png" height="39" width="186" /></a>
-  <div class="sfTMessageContainer sfTAlert">
-    <img alt="page not found" class="sfTMessageIcon" src="<?php echo $path ?>/sf/sf_default/images/icons/tools48.png" height="48" width="48" />
-    <div class="sfTMessageWrap">
-      <h1>Website Temporarily Unavailable</h1>
-      <h5>Please try again in a few seconds... ok?</h5>
-    </div>
-  </div>
-
-  <dl class="sfTMessageInfo">
-    <dt>What's next</dt>
-    <dd>
-      <ul class="sfTIconList">
-        <li class="sfTReloadMessage"><a href="javascript:window.location.reload()">Try again: Reload Page</a></li>
-      </ul>
-    </dd>
-  </dl>
-</div>
+	<div>
+		<div style="float:left; margin-right:510px;">
+			<h2>Сайт на обслуживании</h2>
+			Сайт сейчас находится на обслуживании. Не волнуйтесь, мы скоро вернемся :)
+		</div>
+		<div style="position:absolute;right:0;background:url('/images/disable/simpsons_under_construction.jpg') no-repeat center; width:500px; height:400px;"></div>
+	</div>
 </body>
 </html>
