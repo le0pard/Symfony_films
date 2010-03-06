ba1f30635cf2c7895d1227420068e6e0aed1f658
leo <leo@leo-asus.local>
2010-03-06 16:22:58 +0200

    Design 404

--- apps/frontend/modules/index/templates/error404Success.php
+++ apps/frontend/modules/index/templates/error404Success.php
@@ -1,1 +1,20 @@
-404 ERROR!
+<h2 class="red_h2">Упс! Страница не найдена</h2>
+<div>
+<div class="text404_block_left">
+Страница, которую вы ищете, не найдена.<br /><br /><br />
+Возможно так произошло потому что:
+<ul>
+	<li>Страницу переместили</li>
+	<li>Страница была удалена</li>
+	<li>Вы ищете своего щеночка и потерялись</li>
+	<li>Вам нравятся 404 страницы :)</li>
+</ul>
+<br /><br />
+Или попробуйте поискать по сайту:
+<form action="<?php echo url_for('@search') ?>" method="get">
+<input id="search_input" name="s" type="text" value="" />
+</form>
+</div>
+<div class="not_found_homer"></div>
+</div>
+<div style="clear: both;"></div>
