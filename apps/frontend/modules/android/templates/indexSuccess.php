213c6a1104960288114f6f203028b40a2eb00677
Alexey <leopard.not.a@gmail.com>
2010-08-26 23:21:07 +0300

    Page for android app

---
+++ apps/frontend/modules/android/templates/indexSuccess.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>PopCornUA - Android приложение для афиши кинотеатров Украины</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+    <meta name="description" content="PopCornUA - Android приложение для афиши кинотеатров Украины" />
+    <meta name="keywords" content="popcornua, android, coocoorooza, афиша, кинотеатр, Украина" />
+    <link rel="stylesheet" href="/css/android.css" type="text/css" media="screen" charset="utf-8" />
+</head>
+<body>
+    <div id="top">
+        <div class="center">
+            <h1><img alt="PopCornUA" src="/images/android/logo.gif" width="250" height="63" /></h1>
+
+            <p>Android приложение для афиши кинотеатров Украины.</p>
+            
+            <div id="vid">
+                
+            </div>
+        </div>
+    </div>
+    <div id="band">
+
+        <div class="center">
+            <a id="dl" href="#">Скачать <span class="ver">Версия 1.0</span></a>
+            <div class="preview">
+                <a id="pre" href="#">Страница на AndroidMarket</a>
+            </div>
+        </div>
+
+    </div>
+    <div id="bot">
+        <div class="center">
+            <h2><strong>Основные возможности:</strong></h2>
+              <ol>
+                <li>Возможность работы как онлайн, так и оффлайн (без постоянного подключения к интернету)</li>
+                <li>Просмотр сеансов в выбраном кинотеатре</li>
+                <li>Просмотр фильма и кинотеатров, в которых он транслируется</li>
+                <li>Информация за текущий и следующий день</li>
+              </ol>
+            <div class="bot_line"></div>
+        </div>
+    </div>
+    <div id="ftr">
+        <div class="center">
+
+            
+            <div class="info">
+                <p>              
+                &copy; 2010 <a href="http://leopard.in.ua">Васильев Алексей</a>.
+                </p>
+                
+            </div>
+        </div>
+
+    </div>
+</body>
+</html>
